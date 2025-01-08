559708312e2e729c9789d5bec8d6d0a957bb12a1
Willy Hardian <hardian.will@gmail.com>
2025-01-08 17:20:00 +0700

    add GET /products

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.get("/products", (req, res) => {
+    res.send("products");
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
